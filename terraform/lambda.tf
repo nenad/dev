71566507c6dc0c22fc648c315f105b6b93e7bb3b
Nenad Stojanovikj <nekk1@live.com>
2019-05-07 00:53:50 +0200

    Add lambda function

---
+++ terraform/lambda.tf
@@ -0,0 +1,7 @@
+resource "aws_lambda_function" "lambda_website" {
+  function_name = "handle_request"
+  handler = "main"
+  role = "${aws_iam_role.website_role.arn}"
+  runtime = "go1.x"
+  filename = "website.zip"
+}
